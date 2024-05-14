c19e82acd47573211a7c62d0232199c31eb5b106
colussicode <colussidev@gmail.com>
2024-05-14 10:00:43 +0100

    fixing card layout

--- app/src/main/java/com/comunidadedevspace/imc/CalculateScreen.kt
+++ app/src/main/java/com/comunidadedevspace/imc/CalculateScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -42,6 +43,11 @@ fun CalculateScreen() {
     val height = remember { mutableStateOf("") }
 
     Surface {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .height(20.dp)
+                .background(Primary900)
+        ) {}
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
